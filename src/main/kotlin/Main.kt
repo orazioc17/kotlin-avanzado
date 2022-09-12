9c5bf425e1d49e9deeb8593953ab4cbd6607bad4
Orazio <oraziocappadonna1999@gmail.com>
2022-09-12 10:43:27 -0400

    Funciones de extension

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,32 @@
-fun main(args: Array<String>) {
+import java.util.*
 
+fun main(args: Array<String>) {
+    usoDeFuncionesExtension()
+}
+
+fun usoDeFuncionesExtension() {
+    val string = "esta es una funcion de prueba"
+    println(string.trimAndCapitalize())
+    println(string.charCount())
+
+    val array = intArrayOf(1,2,3,4,5)
+    println(array)
+    println(array.show())
+
+}
+
+fun String.trimAndCapitalize(): String {
+    return this.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }.replace(
+        " ", "")
+}
+
+fun String.charCount(): Int {
+    return this.replace(" ", "").length
+}
+
+fun IntArray.show(): String {
+    var string = "["
+    for (i in this) string += "$i, "
+    string += "]"
+    return string.replace(", ]", "]")
 }
