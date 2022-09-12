8a0317f4a1711400e01dde53e1ccb69be57b78d3
Orazio <oraziocappadonna1999@gmail.com>
2022-09-12 11:28:17 -0400

    High Order Functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,14 @@
 import java.util.*
 
 fun main(args: Array<String>) {
+    println("#### Uso de funciones de extencion ####")
     usoDeFuncionesExtension()
+
+    println()
+    println()
+    println()
+    println("#### Uso de High Order Functions ####")
+    usoHighOrderFunctions()
 }
 
 fun usoDeFuncionesExtension() {
@@ -30,3 +37,25 @@ fun IntArray.show(): String {
     string += "]"
     return string.replace(", ]", "]")
 }
+
+fun usoHighOrderFunctions() {
+    val n1 = 12
+    val n2 = 3
+    // Para pasar las funciones como parametros se les debe poner 2 :: antes del nombre de la funcion
+    println("La suma de $n1 y $n2 es: ${calculadora(n1, n2, ::suma)}")
+    println("La resta de $n1 y $n2 es: ${calculadora(n1, n2, ::resta)}")
+    println("La multiplicacion de $n1 y $n2 es: ${calculadora(n1, n2, ::multiplicacion)}")
+    println("La division de $n1 y $n2 es: ${calculadora(n1, n2, ::division)}")
+}
+
+fun calculadora(n1: Int, n2: Int, fn: (Int, Int) -> Int): Int {
+    return fn(n1, n2)
+}
+
+fun suma (x: Int, y: Int) = x + y
+
+fun resta (x: Int, y: Int) = x - y
+
+fun multiplicacion (x: Int, y: Int) = x * y
+
+fun division (x: Int, y: Int) = x / y
