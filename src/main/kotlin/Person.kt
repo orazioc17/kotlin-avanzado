3cf6aa41b69b0e10828619f77473112354780fc8
Orazio <oraziocappadonna1999@gmail.com>
2022-09-12 12:05:06 -0400

    Clase Person agregada

---
+++ src/main/kotlin/Person.kt
@@ -0,0 +1,9 @@
+class Person(var name: String = "Sin Nombre", var height: Float = 1.6f) {
+
+    fun Person() {
+        name = "Juan"
+        height = 1.6f
+    }
+
+    // fun checkPolice(fn: (Float) -> Boolean) = fn(height)
+}
